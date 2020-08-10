d12074c8751bd34a1ff3a96ca5da347846ed7177
苑龙杰 <yuanlj@kingnet.com>
2020-08-10 15:48:31 +0800

    modify

--- check.php
+++ check.php
@@ -1,11 +1,11 @@
 <?php
-require_once dirname(__FILE__).'/TnCode.class.php';
-$tn  = new TnCode();
-if($tn->check()){
-	$_SESSION['tncode_check'] = 'ok';
+require_once dirname(__FILE__) . '/TnCode.class.php';
+$tn = new TnCode();
+if ($tn->check()) {
+    $_SESSION['tncode_check'] = 'ok';
     echo "ok";
-}else{
-	$_SESSION['tncode_check'] = 'error';
+} else {
+    $_SESSION['tncode_check'] = 'error';
     echo "error";
 }
 
